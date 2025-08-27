652bae9e8789dc28ba1c3d77bb389d062ba15f40
MonishaThobias <monishaklientas@gmail.com>
2025-08-27 13:54:17 +0530

    r

--- PersonalBlog/settings.py
+++ PersonalBlog/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'mypersonalblog-vidv.onrender.com',  # <-- correct, no https:// and no trailing slash
+    'mypersonalblog-vidv.onrender.com',  
 ]
 
 
